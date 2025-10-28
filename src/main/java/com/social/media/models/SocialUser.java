004bee7122356c9808337a6adfb25e9bbcea1e29
Henrique de Antoni <henriqdeantoni@gmail.com>
2025-10-27 22:59:47 -0300

    feat: lombok logic

--- src/main/java/com/social/media/models/SocialUser.java
+++ src/main/java/com/social/media/models/SocialUser.java
@@ -1,6 +1,9 @@
 package com.social.media.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -8,6 +11,9 @@ import java.util.List;
 import java.util.Set;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SocialUser {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
