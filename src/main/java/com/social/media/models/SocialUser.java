f0a1e7f4a3172d25a94a527319af154cfc06cbb5
Henrique de Antoni <henriqdeantoni@gmail.com>
2025-10-27 21:12:24 -0300

    feat: one to one and one to many relations

---
+++ src/main/java/com/social/media/models/SocialUser.java
@@ -0,0 +1,19 @@
+package com.social.media.models;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class SocialUser {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(mappedBy="user")
+    private SocialProfile socialProfile;
+
+    @OneToMany(mappedBy = "socialUser")
+    private List<Post> posts = new ArrayList<>();
+}
